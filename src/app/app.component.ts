be512c4cbf93f41bfe6791da84653f5e21ffb850
faraznaeem <faraznaeem@gmail.com>
2017-08-07 15:22:18 +0200

    add input fields and delete dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Wine Site';
+  handleClick(){
+   alert('Someone clicked on button on the template :-)')
+   }
 }
